b9060deec034f6d80540b26062f896dbceddc47c
fildagomez <fildagomez@seznam.cz>
2021-03-03 16:15:43 +0100

    1st commit

---
+++ binaryReader/AdbConverter.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace binaryReader
+{
+    class AdbConverter
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Altitude { get; set; }
+        public string RoverID { get; set; }
+        public string RoverIP { get; set; }
+        public byte SourceType { get; set; }
+        public string ServiceName { get; set; }
+        public Int16 ServiceType { get; set; }
+        public Int16 ConnectionType { get; set; }
+        public string UserName { get; set; }
+        public int StartWeek { get; set; }
+        public int StartTime { get; set; }
+        public int EndWeek { get; set; }
+        public int EndTime { get; set; }
+        public byte[] Nothing1 { get; set; }
+        public int BytesSent { get; set; }
+        public byte[] Nothing2 { get; set; }
+
+        private string _inputFile;
+        private string _outputFile;
+        private readonly string _dummyFile = Directory.GetCurrentDirectory() + @"\DUMMYOUTPUT.txt";
+
+        public AdbConverter(string inputFile, string outputFile)
+        {
+            _inputFile = Path.GetFullPath(inputFile);
+            _outputFile = Path.GetFullPath(outputFile);
+        }
+
+        public void Process()
+        {
+            //if input file dont exist ask for new path
+            while (!File.Exists(_inputFile))
+            {
+                Console.WriteLine($"ERROR: FILE {_inputFile} DOES NOT EXIST, PLEASE TYPE IN CORRECT PATH");
+                _inputFile = Console.ReadLine();
+            }
+            //if output file doesnt exist create new txt at exe location
+            if (!File.Exists(_outputFile))
+            {
+                Console.WriteLine($"WARNING: FILE {_outputFile} DOES NOT EXIST, CREATING NEW FILE: {_dummyFile}");
+                File.Create(_dummyFile).Close();
+                _outputFile = _dummyFile;
+            }
+            try
+            {
+                using (BinaryReader br = new BinaryReader(new FileStream(_inputFile, FileMode.Open)))
+                {
+                    //throw away header
+                    br.ReadBytes(96);
+
+                    //read bytes and convert them into properties
+                    Latitude = BitConverter.ToDouble(br.ReadBytes(8));
+                    Longitude = BitConverter.ToDouble(br.ReadBytes(8));
+                    Altitude = BitConverter.ToDouble(br.ReadBytes(8));
+                    RoverID = Encoding.ASCII.GetString(br.ReadBytes(255));
+                    RoverIP = Encoding.ASCII.GetString(br.ReadBytes(255));
+                    SourceType = br.ReadByte();
+                    ServiceName = Encoding.ASCII.GetString(br.ReadBytes(255));
+                    ServiceType = BitConverter.ToInt16(br.ReadBytes(2));
+                    ConnectionType = BitConverter.ToInt16(br.ReadBytes(2));
+                    UserName = Encoding.ASCII.GetString(br.ReadBytes(255));
+                    StartWeek = BitConverter.ToInt32(br.ReadBytes(4));
+                    StartTime = BitConverter.ToInt32(br.ReadBytes(4));
+                    EndWeek = BitConverter.ToInt32(br.ReadBytes(4));
+                    EndTime = BitConverter.ToInt32(br.ReadBytes(4));
+                    Nothing1 = br.ReadBytes(32);
+                    BytesSent = BitConverter.ToInt32(br.ReadBytes(4));
+                    Nothing2 = br.ReadBytes(2);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("EXCEPTION THROWN IN PROCESS(): " + e);
+            }
+            finally
+            {
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                //truncate to clear txt before writing
+                using (StreamWriter sw = new StreamWriter(new FileStream(_outputFile, FileMode.Truncate, FileAccess.Write)))
+                {
+                    //replace all null characters to with empty chars
+                    sw.Write(RoverID.Replace("\0", string.Empty));
+                    sw.Write(";");
+                    sw.Write(RoverIP.Replace("\0", string.Empty));
+                    sw.Write(";");
+                    sw.Write(SourceType);
+                    sw.Write(";");
+                    sw.Write(ServiceName.Replace("\0", string.Empty));
+                    sw.Write(";");
+                    sw.Write(ServiceType);
+                    sw.Write(";");
+                    sw.Write(UserName.Replace("\0", string.Empty));
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("EXCEPTION THROWN IN PROCESS(): " + e);
+            }
+            finally
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ binaryReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace binaryReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //check if both arguments were typed
+            if(args.Length == 2)
+            {
+                string inputFile = args[0];
+                string outputFile = args[1];
+                AdbConverter adb = new AdbConverter(inputFile, outputFile);
+                adb.Process();
+                Console.WriteLine("ADB FILE SUCCESSFULY PROCESSED");
+            }
+            //else show information message and close app
+            else
+            {
+                Console.WriteLine("APPLICATION TAKES 2 PARAMETERS [inputFile] [outputFile]");
+            }
+        }
+    }
+}
